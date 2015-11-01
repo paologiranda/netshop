87bf7b104499465d4df1a4b8d44d997ee4cd0de2
luisenri <luisenri86@gmail.com>
2015-11-01 22:42:02 +0100

    Refactor con ConnessioneDB e aggiunta di Test

--- Netshop/netshop-BL/src/main/java/it/netshop/ecommerce/local/GestioneLocal.java
+++ Netshop/netshop-BL/src/main/java/it/netshop/ecommerce/local/GestioneLocal.java
@@ -1,66 +1,61 @@
-//package it.netshop.ecommerce.local;
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import it.netshop.ecommerce.local.dao.DAOLocalita;
-//import it.netshop.ecommerce.local.dao.IDAOLocalita;
-//import it.netshop.ecommerce.local.dto.Comune;
-//import it.netshop.ecommerce.local.dto.Provincia;
-//import it.netshop.ecommerce.local.dto.Regione;
-//
-//public class GestioneLocal {
-//
-//	public IDAOLocalita daolocal;
-//	
-//	public List<Regione> getRegioni(){
-//		List<Regione> regioni = new ArrayList<Regione>();
-//		try {
-//			daolocal = new DAOLocalita();
-//			regioni = daolocal.leggiRegioni();
-//			
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		return regioni;
-//	}
-//	
-//	public List<Provincia> getProvince(int idRegione){
-//		
-//		List<Provincia> provincia = new ArrayList<Provincia>();
-//		
-//		try {
-//			daolocal = new DAOLocalita();
-//			provincia = daolocal.leggiProvince(idRegione);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		return provincia;
-//	}
-//	
-//	public List<Comune> getComuni(int idregione, int idprovincia){
-//		
-//		List<Comune> comuni = new ArrayList<Comune>();
-//		
-//		try {
-//			daolocal = new DAOLocalita();
-//			comuni = daolocal.leggiComuni(idregione, idprovincia);
-//		} catch (SQLException | ClassNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		return comuni;
-//	}
-//		
-//}
+package it.netshop.ecommerce.local;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import it.netshop.ecommerce.local.dao.DAOLocalita;
+import it.netshop.ecommerce.local.dao.IDAOLocalita;
+import it.netshop.ecommerce.local.dto.Comune;
+import it.netshop.ecommerce.local.dto.Provincia;
+import it.netshop.ecommerce.local.dto.Regione;
+
+public class GestioneLocal {
+
+	public IDAOLocalita daolocal;
+	
+	public List<Regione> getRegioni(){
+		List<Regione> regioni = new ArrayList<Regione>();
+		try {
+			daolocal = new DAOLocalita();
+			regioni = daolocal.leggiRegioni();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return regioni;
+	}
+	
+	public List<Provincia> getProvince(int idRegione){
+		
+		List<Provincia> provincia = new ArrayList<Provincia>();
+		
+		try {
+			daolocal = new DAOLocalita();
+			provincia = daolocal.leggiProvince(idRegione);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return provincia;
+	}
+	
+	public List<Comune> getComuni(int idregione, int idprovincia){
+		
+		List<Comune> comuni = new ArrayList<Comune>();
+		
+		try {
+			daolocal = new DAOLocalita();
+			comuni = daolocal.leggiComuni(idregione, idprovincia);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return comuni;
+	}
+		
+}
--- Netshop/netshop-BL/src/main/java/it/netshop/ecommerce/local/dao/DAOLocalita.java
+++ Netshop/netshop-BL/src/main/java/it/netshop/ecommerce/local/dao/DAOLocalita.java
@@ -1,90 +1,85 @@
-//package it.netshop.ecommerce.local.dao;
-//
-//import it.netshop.ecommerce.local.dto.Comune;
-//import it.netshop.ecommerce.local.dto.Provincia;
-//import it.netshop.ecommerce.local.dto.Regione;
-//
-//import java.sql.Connection;
-//import java.sql.DriverManager;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.sql.Statement;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class DAOLocalita extends ADao implements IDAOLocalita {
-//	private static int MAX_REGIONI = 21;
-//	private static String SQLREGIONI = "SELECT * FROM REGIONI WHERE IDREGIONE<"+MAX_REGIONI;
-//
-//	public DAOLocalita() throws ClassNotFoundException {
-//		super();
-//	}
-//
-//	@Override
-//	public List<Regione> leggiRegioni() throws SQLException {
-//
-//		Connection connessione = DriverManager.getConnection(url, "corso",
-//				"corso");
-//		Statement statement = connessione.createStatement();
-//
-//		List<Regione> regioni = new ArrayList<Regione>();
-//
-//		ResultSet rs = statement.executeQuery(SQLREGIONI);
-//
-//		while (rs.next()) {
-//			Regione regione = new Regione();
-//			regione.setId(rs.getInt("IDREGIONE"));
-//			regione.setNome(rs.getString("NOMEREGIONE"));
-//			regioni.add(regione);
-//		}
-//		connessione.close();
-//		return regioni;
-//	}
-//
-//	@Override
-//	public List<Provincia> leggiProvince(int idRegione) throws SQLException {
-//		Connection connessione = DriverManager.getConnection(url, "corso",
-//				"corso");
-//		Statement statement = connessione.createStatement();
-//		String sqlprovince = "SELECT * FROM PROVINCE WHERE IDREGIONE="
-//				+ idRegione;
-//		ResultSet rs = statement.executeQuery(sqlprovince);
-//		List<Provincia> province = new ArrayList<Provincia>();
-//
-//		while (rs.next()) {
-//			Provincia provincia = new Provincia();
-//			provincia.setIdprovincia(rs.getInt("IDPROVINCIA"));
-//			provincia.setIdregione(rs.getInt("IDREGIONE"));
-//			provincia.setNomeprovincia(rs.getString("NOMEPROVINCIA"));
-//			provincia.setSiglaprovincia(rs.getString("SIGLAPROVINCIA"));
-//			province.add(provincia);
-//		}
-//		connessione.close();
-//		return province;
-//	}
-//
-//	@Override
-//	public List<Comune> leggiComuni(int idregione, int idprovincia)
-//			throws SQLException {
-//		Connection connessione = DriverManager.getConnection(url, "corso",
-//				"corso");
-//		Statement statement = connessione.createStatement();
-//		String sqlcomuni = "SELECT * FROM COMUNI where IDREGIONE=" + idregione
-//				+ " AND IDPROVINCIA=" + idprovincia;
-//
-//		ResultSet rs = statement.executeQuery(sqlcomuni);
-//		List<Comune> comuni = new ArrayList<Comune>();
-//
-//		while (rs.next()) {
-//			Comune comune = new Comune();
-//			comune.setId(rs.getInt("ID"));
-//			comune.setIdprovincia(rs.getInt("IDPROVINCIA"));
-//			comune.setNomecomune(rs.getString("NOME"));
-//			comune.setCatasto(rs.getString("CATASTO"));
-//			comuni.add(comune);
-//		}
-//		connessione.close();
-//		return comuni;
-//	}
-//
-//}
+package it.netshop.ecommerce.local.dao;
+
+import it.netshop.db.ConnessioneDB;
+import it.netshop.db.DbUtil;
+import it.netshop.ecommerce.local.dto.Comune;
+import it.netshop.ecommerce.local.dto.Provincia;
+import it.netshop.ecommerce.local.dto.Regione;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOLocalita implements IDAOLocalita {
+	private static int MAX_REGIONI = 21;
+	private static String SQLREGIONI = "SELECT * FROM REGIONI WHERE IDREGIONE<" + MAX_REGIONI;
+
+	@Override
+	public List<Regione> leggiRegioni() throws SQLException {
+
+		Connection connessione = ConnessioneDB.getConnection();
+		Statement statement = connessione.createStatement();
+
+		List<Regione> regioni = new ArrayList<Regione>();
+
+		ResultSet resultSet = statement.executeQuery(SQLREGIONI);
+
+		while (resultSet.next()) {
+			Regione regione = new Regione();
+			regione.setId(resultSet.getInt("IDREGIONE"));
+			regione.setNome(resultSet.getString("NOMEREGIONE"));
+			regioni.add(regione);
+		}
+		DbUtil.close(connessione, statement, resultSet);
+		return regioni;
+	}
+
+	@Override
+	public List<Provincia> leggiProvince(int idRegione) throws SQLException {
+		Connection connessione = ConnessioneDB.getConnection();
+		Statement statement = connessione.createStatement();
+		String sqlprovince = "SELECT * FROM PROVINCE WHERE IDREGIONE="
+				+ idRegione;
+		ResultSet resultSet = statement.executeQuery(sqlprovince);
+		List<Provincia> province = new ArrayList<Provincia>();
+
+		while (resultSet.next()) {
+			Provincia provincia = new Provincia();
+			provincia.setIdprovincia(resultSet.getInt("IDPROVINCIA"));
+			provincia.setIdregione(resultSet.getInt("IDREGIONE"));
+			provincia.setNomeprovincia(resultSet.getString("NOMEPROVINCIA"));
+			provincia.setSiglaprovincia(resultSet.getString("SIGLAPROVINCIA"));
+			province.add(provincia);
+		}
+		DbUtil.close(connessione, statement, resultSet);
+		return province;
+	}
+
+	@Override
+	public List<Comune> leggiComuni(int idregione, int idprovincia)
+			throws SQLException {
+		Connection connessione = ConnessioneDB.getConnection();
+		Statement statement = connessione.createStatement();
+		String sqlcomuni = "SELECT * FROM COMUNI where IDREGIONE=" + idregione
+				+ " AND IDPROVINCIA=" + idprovincia;
+
+		ResultSet resultSet = statement.executeQuery(sqlcomuni);
+		List<Comune> comuni = new ArrayList<Comune>();
+
+		while (resultSet.next()) {
+			Comune comune = new Comune();
+			comune.setIdregione(resultSet.getInt("IDREGIONE"));
+			comune.setId(resultSet.getInt("ID"));
+			comune.setIdprovincia(resultSet.getInt("IDPROVINCIA"));
+			comune.setNomecomune(resultSet.getString("NOME"));
+			comune.setCatasto(resultSet.getString("CATASTO"));
+			comuni.add(comune);
+		}
+		DbUtil.close(connessione, statement, resultSet);
+		return comuni;
+	}
+
+}
